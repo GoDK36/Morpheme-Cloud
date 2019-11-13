caca73fa7a8400c449dcd1cb78b78017821fe717
unknown <siinwoo0306@gmail.com>
2019-11-13 18:00:25 +0900

    error fixed

--- Morpheme_cloud.py
+++ Morpheme_cloud.py
@@ -12,21 +12,27 @@ def flat(content):
 rsc = r'D:\Programming\python\창회선배스터디\Morpheme_Cloud\자료\토지2.txt'
 
 #텍스트 태깅 작업
-tagged_list = []
+tagged_temp = []
 with open(rsc, 'r', encoding="utf8") as kr_f:
     for line in kr_f:
         line = line.strip()
-        tagged_list += flat(line)
+        tagged_temp += flat(line)
 
-for i in tagged_list:
+print(tagged_temp)
+
+tagged_list = []
+
+for i in tagged_temp:
     if '/V' in i:
         i = i.replace('/V','다/V')
         tagged_list.append(tuple(i.split('/')))
     elif '/A' in i:
         i.replace('/A','다/A')
-        tagged_list.append(tuple(i.split('/')))
+        tagged_list.append (tuple (i.split ('/')))
     else:
         tagged_list.append(tuple(i.split('/')))
+
+print(tagged_list)
 
 #내용어 리스트 생성
 word_freq_d = defaultdict(int)
@@ -44,6 +50,12 @@ for morph, tag in tagged_list:
 #폰트 불러오기
 wordcloud = WordCloud(font_path=r"C:\WINDOWS\Fonts\HYBDAL.TTF")
 wordcloud = wordcloud.generate_from_frequencies(word_freq_d)
+# wordcloud = WordCloud(font_path = font_path,
+#     width = 800,
+#     height = 800,
+#     background_color="white",
+#     prefer_horizontal = 0.9999,
+#     min_font_size = 10)
 
 #이미지 사이즈
 fig = plt.figure(figsize=(12,12))

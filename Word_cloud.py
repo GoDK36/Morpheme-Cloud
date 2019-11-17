ee3702373f790b77ee09283a585b8e951f004bb1
GoDK36 <siinwoo0306@gmail.com>
2019-11-18 00:16:44 +0900

    customized Word_Cloud

--- Word_cloud.py
+++ Word_cloud.py
@@ -1,13 +1,11 @@
 import multidict as multidict
 
-import numpy as np
-
 import os
-import re
-from PIL import Image
 from os import path
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
 
 def freq_dict(sentence):
     fullTermsDict = multidict.MultiDict()
@@ -21,26 +19,25 @@ def freq_dict(sentence):
         fullTermsDict.add(key, tmpDict[key])
     return fullTermsDict
 
-rsc = r'D:\Programming\python\창회선배스터디\Morpheme_Cloud\자료'
+rsc = r'E:\Programming\python\창회선배스터디\Morpheme_Cloud\자료'
 
 text = open(path.join(rsc, '토지2.txt'), encoding='utf-8')
 text = text.read()
 txt = freq_dict(text)
+
+star_mask = np.array(Image.open(r"E:\Programming\python\창회선배스터디\Morpheme_Cloud\자료\star_mask.png"))
+
 wc = WordCloud(font_path=r"C:\WINDOWS\Fonts\HYBDAL.TTF",
-               background_color="white",
-               max_words=500,
-               # mask=alice_mask, 워드클라우드의 형태
-               width=800,
-               height = 800,
-               # prefer_horizontal = 0.9999,
-               # min_font_size = 20,
-               # max_font_size= 60,
-               #stopwords=stopwords  불용어 처리
-               )
+		     width=400,
+		     height=200,
+		     prefer_horizontal=0.9999,
+		     mask = star_mask,
+		     background_color = "white",
+		     min_font_size = 1
+)
 wc.generate_from_frequencies(txt)
 
 fig = plt.figure(figsize=(12,12))
-
 plt.imshow(wc, interpolation="bilinear")
 plt.axis("off")
 plt.show()
